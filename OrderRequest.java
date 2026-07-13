bf371fd61da89f890599256d5e752387fc685dc7
Sushreeusharani Sahoo <usharanisahoo2006@gmail.com>
2026-07-13 14:14:07 +0530

    Add files via upload

---
+++ OrderRequest.java
@@ -0,0 +1,13 @@
+package com.example.onlineshop.dto;
+
+public class OrderRequest {
+    private String paymentMethod;
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+}
